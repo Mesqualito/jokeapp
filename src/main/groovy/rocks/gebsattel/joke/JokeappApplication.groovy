ae8eee01f0d1bdae1029ffba46eaa217e7fb8504
Mesqualito <jo@hassfurter.com>
2018-06-14 10:59:40 +0200

    Initial commit after Spring Boot 2.0.2 Initialization with WEB and THYMELEAF

---
+++ src/main/groovy/rocks/gebsattel/joke/JokeappApplication.groovy
@@ -0,0 +1,12 @@
+package rocks.gebsattel.joke
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JokeappApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run JokeappApplication, args
+	}
+}
